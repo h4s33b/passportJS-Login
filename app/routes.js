de4c98335e61291be242b8b534760c224987d4c6
zeeshanshanif <zeeshanhanifpc@gmail.com>
2016-01-26 21:17:24 +0500

    Updated Signup Redirect

--- app/routes.js
+++ app/routes.js
@@ -38,7 +38,7 @@ app.post('/login', passport.authenticate('local-login', {
     });
 
     // process the signup form
-app.post('/signup', passport.authenticate('local-login', {
+app.post('/signup', passport.authenticate('local-signup', {
         successRedirect : '/profile', // redirect to the secure profile section
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
